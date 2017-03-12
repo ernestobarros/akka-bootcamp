aa42ebb4d88bd4f683c722030af9ba4ffc38e051
Ernesto Barros <e.leon.barros@gmail.com>
2017-03-12 13:07:39 +0100

    Lesson 1 - Done

--- src/Unit-1/FSharp/DoThis/Program.fs
+++ src/Unit-1/FSharp/DoThis/Program.fs
@@ -4,32 +4,37 @@ open Akka.FSharp.Spawn
 open Akka.Actor
 open WinTail
 
-let printInstructions () =
+let printInstructions() = 
     Console.WriteLine "Write whatever you want into the console!"
     Console.Write "Some lines will appear as"
     Console.ForegroundColor <- ConsoleColor.Red
     Console.Write " red"
-    Console.ResetColor ()
+    Console.ResetColor()
     Console.Write " and others will appear as"
     Console.ForegroundColor <- ConsoleColor.Green
     Console.Write " green! "
-    Console.ResetColor ()
-    Console.WriteLine ()
-    Console.WriteLine ()
+    Console.ResetColor()
+    Console.WriteLine()
+    Console.WriteLine()
     Console.WriteLine "Type 'exit' to quit this application at any time.\n"
 
 [<EntryPoint>]
-let main argv = 
+let main _ = 
     // initialize an actor system
     // YOU NEED TO FILL IN HERE
-    
-    printInstructions ()
-    
+    let myActorSystem = 
+        System.create "MyActorSystem" (Configuration.parse "akka.suppress-json-serializer-warning = on")
+
+    printInstructions()
+
     // make your first actors using the 'spawn' function
     // YOU NEED TO FILL IN HERE
+    let consoleWriterActor = spawn myActorSystem "consoleWriterActor" <| actorOf (Actors.consoleWriterActor)
+    let consoleReaderActor = 
+        spawn myActorSystem "consoleReaderActor" <| actorOf2 (Actors.consoleReaderActor consoleWriterActor)
 
     // tell the consoleReader actor to begin
     // YOU NEED TO FILL IN HERE
-
-    myActorSystem.AwaitTermination ()
-    0
+    consoleReaderActor <! Actors.Start
+    myActorSystem.WhenTerminated.Wait()
+    0 // return an integer exit code
