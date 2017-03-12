b3ce016982d99e896de7b61ef0ba5be41d900775
Ernesto Barros <e.leon.barros@gmail.com>
2017-03-12 20:08:18 +0100

    Lesson 1.2 - Done

---
+++ src/Unit-1/FSharp/DoThis/Messages.fs
@@ -0,0 +1,17 @@
+﻿namespace WinTail
+
+[<AutoOpen>]
+module Messages =
+    type Command =
+        | Start
+        | Continue
+        | Message of string
+        | Exit
+
+    type ErrorType =
+        | Null
+        | Validation
+
+    type InputResult =
+        | InputSuccess of string
+        | InputError of reason : string * errorType : ErrorType
